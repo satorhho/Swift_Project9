7cdc838b2030f591df8e14c02c49341bd67ed015
satorhho <lbriones@gbox.adnu.edu.ph>
2020-05-10 00:09:45 +0800

    Circle color spectrum

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,58 +8,42 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
+struct ColorCycingCircle: View {
+    var amount = 0.0
+    var steps = 125
     
-    // How wide to make each petal
-    var petalWidth: Double = 100
+    var body: some View {
+        ZStack{
+            ForEach(0..<steps){ value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+                
+            }
+        }
+        .drawingGroup()
+    }
     
-    func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
-        var path = Path()
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
         
-        // Count from 0 up to 2*pi, moving up by pi/8 each time
-        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi/8){
-            // Rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-            
-            // Move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-            
-            // Create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width/2))
-            
-            // Apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-            
-            // Add it to our path
-            path.addPath(rotatedPetal)
-            
+        if targetHue > 1 {
+            targetHue -= 1
         }
         
-        // Now send the main path back
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
-    @State private var petalOffset: Double = -20.0
-    @State private var petalWidth: Double = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack{
-            Flower(petalOffset: self.petalOffset, petalWidth: self.petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
+            ColorCycingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
             
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in:0...100)
-                .padding(.horizontal)
-            
+            Slider(value: $colorCycle)
         }
     }
 }
